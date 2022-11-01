379a8a911b0c6988fa3747d348e1844754e37943
Hans-Olivier.Fontaine <fonh3001@usherbrooke.ca>
2022-11-01 16:45:12 -0400

    preprocessing.py

--- problematique/preprocessing.py
+++ problematique/preprocessing.py
@@ -13,28 +13,13 @@ def edge_coefficient(image):
     return edges_high_thresh.mean()
 
 
-def warm_cold_color_ratio(image):
-    # TODO: ocean (présente de couleurs très chaudes dans la partie supérieure:
-    #  en divisant verticalement l'image, la saturation des pixels de la moitié
-    #  supérieure est au moins 30% plus basse et l'intensité lumineuse est 25% plus
-    #  élevée que la moitié inférieure seulement si l'image est un océan
-    pass
-
-
-def asphalt_pixels(image):
-    # TODO: ville (présence de tons de gris dans le tier inférieur:
-    #  présence d’un grand nombre de pixels à basse saturation et valeur
-    #  variante dans le tier le plus bas dans le domaine HSV)
-    pass
-
-
-def hog_factor(image):
+def hog_factor(image):  # legacy
     fd, hog_image = hog(image, orientations=9, pixels_per_cell=(8, 8),
                         cells_per_block=(2, 2), visualize=True, multichannel=True)
     return np.sum(hog_image)
 
 
-def top_down_luminosity_ratio(image):
+def top_down_luminosity_ratio(image):   # legacy
     shape = image.shape
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     top = image[:int(shape[0] / 2), :, :]
@@ -42,7 +27,7 @@ def top_down_luminosity_ratio(image):
     return np.average(top[:, :, 2]) / np.average(bottom[:, :, 2])
 
 
-def left_right_color_difference_ratio(image):
+def left_right_color_difference_ratio(image):   # legacy
     shape = image.shape
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     left = image[:, :int(shape[0] / 2), :]
@@ -59,6 +44,7 @@ def h_dominant_gradient(image):
     # cv2.imshow('D', ims)
     # cv2.waitKey()
     return abs(dx).sum() / abs(dy).sum()
+
 
 def very_grey(image, limit=15):
     # Grey is where r, g and b are all equal, this functions gives a little wiggle room
@@ -90,19 +76,6 @@ def leaf_color_coef(img):
     return np.count_nonzero(res) / res.flatten().size
 
 
-# def excess_green(image):
-#     r = np.sum(image[:, :, 0])
-#     g = np.sum(image[:, :, 1])
-#     b = np.sum(image[:, :, 2])
-#
-#     return g / (r + g + b)
-
-def normalize(image):
-    total = (image[:, :, 0] + image[:, :, 1] + image[:, :, 2])
-    total = np.where(total == 0, 1, total)
-    return image[:, :, 0] / total, image[:, :, 1] / total, image[:, :, 2] / total
-
-
 def excess_green_index(image):
     red, green, blue = image[:, :, 0], image[:, :, 1], image[:, :, 2]  # normalize(image)
     r = np.sum(red)
@@ -111,7 +84,7 @@ def excess_green_index(image):
     return g / (r + b + g)
 
 
-def excess_blue_index(image):
+def excess_blue_index(image):   # legacy
     r, g, b = image[:, :, 0], image[:, :, 1], image[:, :, 2]
     r = np.sum(r)
     g = np.sum(g)
@@ -125,10 +98,3 @@ def excess_red_index(image):
     g = np.sum(g)
     b = np.sum(b)
     return r / (r + g + b)
-
-# def excess_greenred_index(image):
-#     image = normalize(image)
-#     r = np.sum(image[:, :, 0])
-#     g = np.sum(image[:, :, 1])
-#     b = np.sum(image[:, :, 2])
-#     return (3 * g) - ((2.4 * r) - b)
